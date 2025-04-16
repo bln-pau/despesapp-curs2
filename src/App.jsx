1bf4c527cd3c45275b2499f1a6c5e5265c63a63a
Irene <ivinentmercadal@iesjoanramis.org>
2025-04-16 12:01:51 +0200

    Fi sessió 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
+import { useState } from 'react';
 import './App.css';
+import Titol from './components/titol/Titol';
 
 function App() {
-  
-  const titol = "Benvinguts al Curs";
-  const subTitol = "React & Firebase";
-  const web = "http://www.menorca.info";
+  const [mostrarDespeses, setMostrarDespeses] = useState(true);
+
+  const [despeses, setDespeses] = useState([
+    {concepte: "dinar", quantia: 30.55, pagatPer: "Pere", id: 1},
+    {concepte: "sopar", quantia: 45.65, pagatPer: "Toni", id: 2},
+    {concepte: "excursió", quantia: 21.33, pagatPer: "Anna", id: 3}
+  ]);
+
+  //console.log(mostrarDespeses);
+
+  const subtitol = "React & Firebase!!";
+
+  const handleClick = (id) => {
+    setDespeses((despesesPrevies)=> {
+      return despesesPrevies.filter((despesa) => id !== despesa.id)
+    })
+
+  }
 
   return (
     <div>
-      <h2>{ titol }</h2>
-      <p>{ subTitol }</p>
-      <p>La data d'avui és: {new Date().toDateString() }</p>
-      <p>Número aleatòri: { Math.random() * 100 }</p>
-      <a href={ web }>Visita Es Diari</a>
+      <Titol titol="Benvinguts al Curs!!" subtitol={subtitol} />
+      { !mostrarDespeses &&
+      (
+      <div>
+        <button onClick={()=>setMostrarDespeses(true)}>Mostrar Despeses</button>
+      </div>
+      )
+      }
+      { mostrarDespeses &&
+      (
+      <div>
+        <button onClick={()=>setMostrarDespeses(false)}>Ocultar Despeses</button>
+      </div>
+      )
+      }
+      {mostrarDespeses &&
+        despeses.map((despesa, index)=>(
+          <div key={despesa.id}>
+            <h2>{index + 1} - {despesa.concepte}</h2>
+            <button onClick={() => handleClick(despesa.id)}>Eliminar Despesa</button>
+          </div>
+        )
+      )
+      }
     </div>
   )
 }
