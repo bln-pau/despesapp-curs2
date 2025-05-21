7a97d56073f0a9255d81c5c67b8506bcafc9224e
Irene <ivinentmercadal@iesjoanramis.org>
2025-05-21 10:56:37 +0200

    Un poc de css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom"
+import './Navbar.css'
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="navbar">
         <ul>
+        <li className="titol"><Link to="/">Despesapp</Link></li>
         <li><Link to="/">Inici</Link></li>
         <li><Link to="/login">Login</Link></li>
         </ul>
